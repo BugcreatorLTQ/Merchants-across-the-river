043429f5008939cc1fc2db7f5ba722cb8723e709
Lain <2649256918@qq.com>
2018-04-14 19:16:49 +0800

    last change maybe

--- List.h
+++ List.h
@@ -1,36 +1,45 @@
 
-typedef struct node {
+typedef struct node
+{
 	Vector data;
 	int flag;
 	node *next;
-}*Node;
+} * Node;
 
-class List {
-private:
+class List
+{
+  private:
 	Node head;
 	Node rear;
-	int count;
-public:
-	List() { head = new node; rear = head; head->next = NULL; count=0; };
-	~List();//destructor
+
+  public:
+	List()
+	{
+		head = new node;
+		rear = head;
+		head->next = NULL;
+	};
+	~List(); //destructor
 	List(const List &other);
-	void Insert(Vector &action,int n);
-	void Show(const List &other);//Show this List other is actions
-	void Delete();//Delete last node
-	bool IsLoop(Vector &action, int n);//judging dead Loops
+	void Insert(Vector &action, int n);
+	void Show(const List &other);		//Show this List other is actions
+	void Delete();						//Delete last node
+	bool IsLoop(Vector &action, int n); //judging dead Loops
 };
 
-List::~List() {
+List::~List()
+{
 	Node now = head, next;
-	cout << count << endl;
-	while (now != NULL) {
+	while (now != NULL)
+	{
 		next = now->next;
 		delete now;
 		now = next;
 	}
 }
 
-void List::Insert(Vector &action,int n) {
+void List::Insert(Vector &action, int n)
+{
 	Node now = new node;
 	now->data = action;
 	if (n % 2 == 1)
@@ -40,13 +49,14 @@ void List::Insert(Vector &action,int n) {
 	rear->next = now;
 	rear = now;
 	now->next = NULL;
-	count++;
 }
 
-void List::Show(const List &solution) {
+void List::Show(const List &solution)
+{
 	Node now = head->next;
 	Node action = solution.head->next;
-	while (action != NULL) {
+	while (action != NULL)
+	{
 		now->data.ShowResult();
 		action->data.ShowSolution();
 		now = now->next;
@@ -55,20 +65,23 @@ void List::Show(const List &solution) {
 	now->data.ShowResult();
 }
 
-void List::Delete(){
+void List::Delete()
+{
 	Node now = head;
-	while (now->next != rear) {
+	while (now->next != rear)
+	{
 		now = now->next;
 	}
 	delete rear;
 	now->next = NULL;
 	rear = now;
-	count--;
 }
 
-bool List::IsLoop(Vector &action,int n) {
+bool List::IsLoop(Vector &action, int n)
+{
 	Node now = head->next;
-	while (now != NULL) {
+	while (now != NULL)
+	{
 		if (now->data == action)
 			if (n % 2 != now->flag)
 				return false;
